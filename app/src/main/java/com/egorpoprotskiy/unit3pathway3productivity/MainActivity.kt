93a78378376596445718295fc4a92447df6bc9cf
Egor Poprotskiy <egorpoprotskiy@gmail.com>
2025-01-11 03:46:50 +1000

    Initial commit

---
+++ app/src/main/java/com/egorpoprotskiy/unit3pathway3productivity/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.egorpoprotskiy.unit3pathway3productivity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.egorpoprotskiy.unit3pathway3productivity.ui.theme.Unit3Pathway3ProductivityTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Unit3Pathway3ProductivityTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Unit3Pathway3ProductivityTheme {
+        Greeting("Android")
+    }
+}
