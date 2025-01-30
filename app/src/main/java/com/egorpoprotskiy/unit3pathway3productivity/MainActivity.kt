6d2176e872d8597da10263ac9535b47570b74cf8
Egor Poprotskiy <egorpoprotskiy@gmail.com>
2025-01-31 04:34:16 +1000

    4_create_list

--- app/src/main/java/com/egorpoprotskiy/unit3pathway3productivity/MainActivity.kt
+++ app/src/main/java/com/egorpoprotskiy/unit3pathway3productivity/MainActivity.kt
@@ -10,13 +10,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.calculateStartPadding
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
